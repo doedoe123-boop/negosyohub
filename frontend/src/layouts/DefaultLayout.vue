d0d275e1fe9ffeda6f4521b578cdd4c2d830bf53
Nelson Asumbrado <84433704+doedoe123-boop@users.noreply.github.com>
2026-03-17 13:53:42 +0800

    Develop (#26)
    * added marketing resources on admin panel
    
    * fix security issues found
    
    * fix test admin path
    
    * fix docker file to add healthcheck
    
    * update docker security scanner with correct version on trivy action
    
    * update phasing docs and as well as other docs related to the repository
    
    * remove redundant docs
    
    * update layouts on the frontend
    
    * added integration marketing resources on the storefront
    
    * fix container issue and makefile update
    
    * fix media library on real estate sector
    
    * refine the layout design of real estate page add missing information
    
    * fix makefile
    
    * added vite test instead of playwright (#14)
    
    * update docker and ci actions
    
    * modify the template of rental sector
    
    * added rental info and modify listing page
    
    * added new feature on customer facing for sending inquiries
    
    * fix the image issue pull the correct fpm version
    
    * added rental agreement and notification
    
    * added support ticket on customer
    
    * added staff resource on the admin
    
    * added tests
    
    * added seo and meta tags resources (#24)
    
    * fix issue on head tag
    
    * added firebase and update dockerfile to use frankenphp
    
    * added health check on dockerfile

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from "vue";
+import { computed, onMounted } from "vue";
 import { RouterView } from "vue-router";
 import { useHead } from "@unhead/vue";
 import Navbar from "@/components/Navbar.vue";
