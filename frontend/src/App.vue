e84b561511fdf62412c0486e32207f0d3163ac8f
Nelson <asum.nelson@gmail.com>
2026-03-27 08:02:41 +0800

    added speed insights

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
 import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
   <Analytics />
+  <SpeedInsights />
   <RouterView />
 </template>
